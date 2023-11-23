dcb961f5499ef4a0d9ea9ffda3cd7c21d9623093
Aleksei Kanaev <kingkonu@me.com>
2023-11-23 22:57:02 +0300

    Initial Commit

---
+++ BlueRecipes/BlueRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueRecipesApp.swift
+//  BlueRecipes
+//
+//  Created by Alexey Kanaev on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BlueRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueRecipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueRecipes
+//
+//  Created by Alexey Kanaev on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

47a198b78fb0a3411450b78c0fa3c187b4574d05
Aleksei Kanaev <kingkonu@me.com>
2023-12-23 20:27:34 +0300

    Create @Model for SwiftData in Recipe file

--- BlueRecipes/BlueRecipesApp.swift
+++ BlueRecipes/BlueRecipesApp.swift
@@ -5,6 +5,7 @@
 //  Created by Alexey Kanaev on 11/23/23.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BlueRecipesApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Recipe.self)
     }
 }
--- BlueRecipes/Model/Recipe.swift
+++ BlueRecipes/Model/Recipe.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Recipe {
+    let id = UUID()
+    let name: String
+    let describe: String
+    let ingredients: String
+    let cooking: String
+    var isFavorite: Bool
+
+    init(name: String = "", describe: String = "", ingredients: String = "", cooking: String = "", isFavorite: Bool = false) {
+        self.name = name
+        self.describe = describe
+        self.ingredients = ingredients
+        self.cooking = cooking
+        self.isFavorite = isFavorite
+    }
+}

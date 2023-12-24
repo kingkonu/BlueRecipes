83d2c3e704c8048fce5f9711615e37a3d864d59e
Aleksei Kanaev <kingkonu@me.com>
2023-12-24 20:05:29 +0300

    Create func addRecipe in ContentView

--- BlueRecipes/ContentView.swift
+++ BlueRecipes/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = [Recipe]()
     @Environment(\.modelContext) var modelContext
     @Query var recipes: [Recipe]
-    
+
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(recipes) { recipe in
                     NavigationLink(value: recipe) {
@@ -29,17 +30,18 @@ struct ContentView: View {
             .navigationTitle("Рецепты")
             .navigationDestination(for: Recipe.self, destination: EditRecipeView.init)
             .toolbar {
-                Button("Add samples", action: addSamples)
+                Button("Добавить рецепт", systemImage: "plus", action: addRecipe)
             }
         }
     }
+
     
-    func addSamples() {
-        let soup = Recipe(name: "Суп")
-        
-        modelContext.insert(soup)
+    func addRecipe() {
+        let recipe = Recipe()
+        modelContext.insert(recipe)
+        path = [recipe]
     }
-    
+
     func deleteRecipe(_ indexSet: IndexSet) {
         for index in indexSet {
             let recipe = recipes[index]

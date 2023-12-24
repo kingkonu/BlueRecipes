e9257467883ed9b7ecef292a36f8a16f12137752
Aleksei Kanaev <kingkonu@me.com>
2023-12-24 19:48:26 +0300

    Create EditRecipeView file

---
+++ BlueRecipes/EditRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  EditRecipeView.swift
+//  BlueRecipes
+//
+//  Created by Alexey Kanaev on 12/24/23.
+//
+
+import SwiftUI
+
+struct EditRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditRecipeView()
+}

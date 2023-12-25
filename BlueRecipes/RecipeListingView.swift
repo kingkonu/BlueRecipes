a186fdc98487ba3b07c2b8ebba0e0f4dd2ae841c
Aleksei Kanaev <kingkonu@me.com>
2023-12-25 21:38:26 +0300

    Create Sorting function in new RecipeListingView

---
+++ BlueRecipes/RecipeListingView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListingView.swift
+//  BlueRecipes
+//
+//  Created by Alexey Kanaev on 12/25/23.
+//
+
+import SwiftUI
+
+struct RecipeListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListingView()
+}
